5de0652b1759b28196d84c0271c47467c06adb34
arkdich <artem002498@gmail.com>
2021-06-05 20:09:56 +0600

    seems overcomplicated but funny lol

--- 03_pig_game/js/script.js
+++ 03_pig_game/js/script.js
@@ -11,6 +11,7 @@ const player1 = { current: 0, total: 0 };
 const player2 = { current: 0, total: 0 };
 
 let currentPlayer = 0;
+let isPlayable = true;
 
 function startNew() {
   player1.current =
@@ -30,9 +31,13 @@ function resetGame() {
   currents.forEach((c) => (c.innerText = "0"));
   totals.forEach((t) => (t.innerText = "0"));
 
+  updateImg(-1);
+  isPlayable = true;
+
   const cols = document.querySelectorAll(".game-body__col");
   cols[0].classList.add("active");
-  cols[1].classList.remove("active");
+  cols[0].classList.remove("game-body_win-state");
+  cols[1].classList.remove("active", "game-body_win-state");
 }
 
 function checkAndDo(callback) {
@@ -40,10 +45,13 @@ function checkAndDo(callback) {
 }
 
 function rollDice(thisArg) {
-  roll.apply(thisArg);
+  if (isPlayable) {
+    roll.apply(thisArg);
+  }
 
   function roll() {
     let dice = Math.ceil(Math.random() * 6);
+    updateImg(dice);
 
     if (dice == 1) {
       this.current = 0;
@@ -58,12 +66,20 @@ function rollDice(thisArg) {
 }
 
 function holdTurn(thisArg) {
-  hold.apply(thisArg);
+  if (isPlayable) {
+    hold.apply(thisArg);
+  }
 
   function hold() {
     this.total += this.current;
     this.current = 0;
     updateStats.apply(thisArg);
+
+    if (this.total >= 100) {
+      gameWinState();
+      return;
+    }
+
     switchPlayer();
   }
 }
@@ -76,10 +92,28 @@ function updateStats() {
   totalStats.innerText = this.total;
 }
 
+function updateImg(diceNum) {
+  const rollImg = document.querySelector(".dice img");
+
+  if (diceNum < 1) {
+    rollImg.src = "";
+    return;
+  }
+
+  rollImg.src = `imgs/dice-${diceNum}.png`;
+}
+
 function switchPlayer() {
   document
     .querySelectorAll(".game-body__col")
     .forEach((col) => col.classList.toggle("active"));
 
   currentPlayer = currentPlayer === 0 ? 1 : 0;
+}
+
+function gameWinState() {
+  const winner = document.querySelector(".active");
+  winner.classList.add("game-body_win-state");
+
+  isPlayable = false;
 }
